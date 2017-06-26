4b7ecb95a456f0a8ce1f08284e4192888f3eafb7
ZhangDan062 <1071102546@qq.com>
2017-06-26 20:10:57 +0800

    准备练习OJ

---
+++ OJ_C/OJ_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OJ_C
+//
+//  Created by ZD-Mac on 2017/6/26.
+//  Copyright © 2017年 ZD-Mac. All rights reserved.
+//
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+}
