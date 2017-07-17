960dd1b249ca2bcfcf110799c7dd5f80e4fa5a79
ZhangDan062 <1071102546@qq.com>
2017-07-17 17:38:33 +0800

    只能执行一次，每次都需要重新编译

--- OJ_C/OJ_C/Exponentiation.c
+++ OJ_C/OJ_C/Exponentiation.c
@@ -9,23 +9,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define dig 1000
+int len;
+int product[126]={0};
+void multiply(int a[],int n){
+    int i;
+    int carry=0;
+    for (i=0; i<len; i++) {
+        int temp=a[i]*n+carry;
+        a[i]=temp%10;
+        carry=temp/10;
+    }
+    while (carry) {
+        a[i++]=carry%10;
+        carry/=10;
+    }
+    len=i;
+}
 int main(){
     char s[6];
-    char *end;
-    char *rs[6];
     int n=0;
-    for (int i=0; i<2; i++) {
-        scanf("%s%d",s,&n);
-        double num=strtod(s,&end);//s--double
-        double result=1;
-        for (int j=0; j<n; j++) {
-            result*=num;
+    //while (scanf("%s %d",s,&n)!=EOF) {
+    scanf("%s %d",s,&n);
+        int position=0,i=0,j=0,num=0;
+        for (i=0; i<strlen(s); i++) {
+            if (s[i]=='.') {
+                position=(int)(strlen(s)-i-1)*n;
+            } else {
+                num=num*10+s[i]-48;
+            }
         }
-        gcvt(result, dig, rs[i]);//double--s
-    }
-    for (int i=0; i<2; i++) {
-        printf("%s",rs[i]);
-    }
+        product[0]=1;
+        len=1;
+        for (i=0; i<n; i++) {
+            multiply(product,num);
+        }
+        if (len<=position) {
+            printf(".");
+            for (i=0; i<position-len; i++) {
+                printf("0");
+            }
+            j=0;
+            for (i=len-1; i>=j; i--) {
+                printf("%d",product[i]);
+            }
+        }
+        else{
+            j=0;
+            while (product[j]==0&&j<position) {
+                j++;
+            }
+            for (i=len-1; i>=j; i--) {
+                if(i+1==position){
+                    printf(".");
+                }
+                printf("%d",product[i]);
+            }
+        }
+        printf("\n");
+    //}
     return 0;
 }
